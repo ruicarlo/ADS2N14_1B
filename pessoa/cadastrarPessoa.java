980fbfdd34e55646d3512e8e04c0bb12155cc58a
Rui Carlos do Carmo Rogerio <ruicarlos@r8solucoes.com.br>
2014-02-25 21:53:56 -0300

    Importacao estatica do system

--- pessoa/cadastrarPessoa.java
+++ pessoa/cadastrarPessoa.java
@@ -3,6 +3,8 @@ package pessoa;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+import static java.lang.System.*;
+
 public class cadastrarPessoa {
 
     public static void main(String[] args) {
@@ -18,60 +20,60 @@ public class cadastrarPessoa {
         int idade;
         boolean idadeValidada = false;
         
-        Scanner ler = new Scanner(System.in);
+        Scanner ler = new Scanner(in);
         Pessoa pessoa = new Pessoa();
 
-        System.out.print("Digite seu nome: ");
+        out.print("Digite seu nome: ");
         nome = ler.next();
         pessoa.setNome(nome);
 
-        System.out.print("Digite seu RG: ");
+        out.print("Digite seu RG: ");
         RG = ler.next();
         pessoa.setRG(RG);
 
         do {
             try {
-                System.out.print("Digite seu CPF: ");
+                out.print("Digite seu CPF: ");
                 CPF = ler.next();
                 pessoa.validarCPF(CPF);
                 pessoa.setCPF(CPF);
                 CPFValidado = true;
             } catch(Exception Excecao) {
-                System.out.println(Excecao.getMessage());
+                out.println(Excecao.getMessage());
             }
         } while(!CPFValidado);
 
         do {
             try {
-                System.out.print("Digite seu sexo (M ou F): ");
+                out.print("Digite seu sexo (M ou F): ");
                 genero = ler.next();
                 pessoa.validarGenero(genero);
                 pessoa.setGenero(genero);
                 generoValidado = true;
             } catch(Exception Excecao) {
-                System.out.println(Excecao.getMessage());
+                out.println(Excecao.getMessage());
             }
         } while(!generoValidado);
 
         do {
             try {
-                System.out.print("Digite sua idade: ");
+                out.print("Digite sua idade: ");
                 idade  = ler.nextInt();
                 pessoa.validarIdade(idade);
                 pessoa.setIdade(idade);
                 idadeValidada = true;
             } catch(InputMismatchException e) {
-                System.out.println("Idade inválida");
-                ler = new Scanner(System.in);
+                out.println("Idade inválida");
+                ler = new Scanner(in);
             } catch(Exception e) {
-                System.out.println(e.getMessage());
+                out.println(e.getMessage());
             }
         } while(!idadeValidada);
         
-        System.out.println("Nome: " + pessoa.getNome());
-        System.out.println("CPF: " + pessoa.getCPF());
-        System.out.println("RG: " + pessoa.getRG());
-        System.out.println("Genero: " + pessoa.getGenero());
-        System.out.println("Idade: " + pessoa.getIdade());
+        out.println("Nome: " + pessoa.getNome());
+        out.println("CPF: " + pessoa.getCPF());
+        out.println("RG: " + pessoa.getRG());
+        out.println("Genero: " + pessoa.getGenero());
+        out.println("Idade: " + pessoa.getIdade());
     }    
 }
