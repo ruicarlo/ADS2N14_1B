5f0f5689f49c5de4105f52e93f295b306a5477c7
ruicarlo <ruicarlos@r8solucoes.com.br>
2014-03-17 20:05:21 -0300

    Atualização para buscar nome e sobrenome aleatório
    Atualização para buscar nome e sobrenome aleatório

--- pessoa/controller/PessoaController.java
+++ pessoa/controller/PessoaController.java
@@ -42,9 +42,9 @@ public class PessoaController {
     }
     
     public void gerarCadastroAleatorioT2() {
-        String nomes[] = {"Miguel","Gabriel","Lucas","Gustavo","Felipe","Enzo","João Pedro","Cauã","Eduardo","Henrique","Vinicius","Pietro","Leonardo","Heitor","Isaac","Thiago","João","Bruno","Carlos Eduardo","Breno","Ryan","Yuri","Erick","Fernando","André","Lívia","Helena","Lorena","Larissa","Heloisa","Melissa","Eduarda","Rebeca","Alícia","Lavínia","Ester","Emily","Cecília","Pietra","Milena","Laís","Maria","Camila","Ana Laura","Maria Vitória","Olivia","Mirella","Stella","Stefany","Isabel"};
-        for (String nome2 : nomes) {
-            this.cadastrarPessoaT2(nome2, this.gerarTelefoneAleatorioT2());
+        String nomeAleatorio = getNomeAleatorio() + " " + getSobrenomeAleatorio();
+        for(int i = 0; i < 50; i++) {
+            this.cadastrarPessoaT2(nomeAleatorio, this.gerarTelefoneAleatorioT2());
             this.view.imprimeDadosPessoa(this.pessoa.getNome(), this.pessoa.getTelefone());        
         }
     }
@@ -169,5 +169,15 @@ public class PessoaController {
                 out.println(e.getMessage());
             }
         } while(!this.telefoneValidado);    
+    }
+
+    private String getNomeAleatorio() {
+        String nomes[] = {"MIGUEL","GABRIEL","LUCAS","GUSTAVO","FELIPE","ENZO","JOÃO PEDRO","CAUÃ","EDUARDO","HENRIQUE","VINICIUS","PIETRO","LEONARDO","HEITOR","ISAAC","THIAGO","JOÃO","BRUNO","CARLOS EDUARDO","BRENO","RYAN","YURI","ERICK","FERNANDO","ANDRÉ","LÍVIA","HELENA","LORENA","LARISSA","HELOISA","MELISSA","EDUARDA","REBECA","ALÍCIA","LAVÍNIA","ESTER","EMILY","CECÍLIA","PIETRA","MILENA","LAÍS","MARIA","CAMILA","ANA LAURA","MARIA VITÓRIA","OLIVIA","MIRELLA","STELLA","STEFANY","ISABEL"};
+        return nomes[gerador.nextInt(nomes.length)]; 
+    }
+
+    private String getSobrenomeAleatorio() {
+        String sobrenomes[] = {"ABROMOVIZT","ABROMOVIZT FILHO","ABUEEAR","AGUIAR","AHID","GASPAROTO","ALBERS","ALBERT","ALBERTON","ALBINO","ALENCAR","ALFARO","ALFLEN","ALLEIN","ALMEIDA","ALTENBURG","ALTHOFF","ALVES","ALVES PANTOJA","AMARAL","AMBRÓSIO","AMORIM","AMORIM NETO","ANDRADE","ANDRADE FILHO","AZAMBUJA","BACKES","BADO","BALDESSIN","BALTAZAR","BAPTISTA","BARBOSA","BARDEN","BARDINI","BARNI","BARRA","BARRETO","BARRETO FILHO","BARROS","BARTH","BARTHES","BASON","BASTOS","BATISTA","BAUMER","BAUNGARTEN","BAYER","BECHER","BIANCHINI","BIANKM"};
+        return sobrenomes[gerador.nextInt(sobrenomes.length)]; 
     }
 }
